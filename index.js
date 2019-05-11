46df744815d218d041fc4626dde08b5979d685a1
Surya Kant Bansal <skb1129@yahoo.com>
2019-05-12 00:49:38 +0530

    android setup tested working on ios

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 import App from './App';
-import {name as appName} from './app.json';
+import {name} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`navigation.${name}`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => (
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: `navigation.${name}`
+      }
+    }
+  })
+));
--- ios/rnapp/AppDelegate.m
+++ ios/rnapp/AppDelegate.m
@@ -17,19 +17,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"rnapp"
-                                            initialProperties:nil];
   
   [ReactNativeNavigation bootstrap:[self sourceURLForBridge: bridge] launchOptions:launchOptions];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
   return YES;
 }
 
